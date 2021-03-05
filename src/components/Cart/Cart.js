44c97ec4ba41bcfc4555f743dae069a86004f1d1
Asif2535 <aiqbal517@gmail.com>
2021-03-05 11:55:28 +0600

    complete order place

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,31 +1,30 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const Cart = (props) => {
     const cart = props.cart;
+
     // console.log(cart);
 
-    const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+    // const total = cart.reduce((total, prd) => total + prd.price, 0);
 
-    // let total= 0;
-    // for(let i=0; i< cart.length; i++){
-    //     const product = cart[i];
-    //     total = total + product.length;
-    // }
-
+    let total= 0;
+    for(let i=0; i< cart.length; i++){
+        const product = cart[i];
+        total = total + product.price * product.quantity;
+    }
     let shipping = 0;
-    if (totalPrice > 35) {
+    if (total > 35) {
         shipping = 0;
     }
-    else if (totalPrice > 15) {
+    else if (total > 15) {
         shipping = 4.99;
     }
-    else if (totalPrice > 0) {
+    else if (total > 0) {
         shipping = 12.99;
     }
 
-    const tax = totalPrice / 10;
-    const grandTotal = (totalPrice + shipping + Number(tax)).toFixed(2);
+    const tax = total / 10;
+    const grandTotal = (total + shipping + Number(tax)).toFixed(2);
 
     const formatNumber = num => {
         const precision = num.toFixed(2);
@@ -35,14 +34,14 @@ const Cart = (props) => {
         <div>
             <h4>Order Summary</h4>
             <p>Items ordered: {cart.length}</p>
-            <p>Product Price: {formatNumber(totalPrice)}</p>
+            <p>Product Price: {formatNumber(total)}</p>
             <p><small>Shipping Cost: {shipping}</small></p>
             <p><small>Tax + Vat: {formatNumber(tax)}</small></p>
             <p>Totla Price: {grandTotal}</p>
             <br />
-            <Link to="/review">
-                <button className="main-button">Review Order</button>
-            </Link>
+            {
+                props.children
+            }
         </div>
     );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,11 +1,26 @@
 import React, { useEffect, useState } from 'react';
 import fakeData from '../../fakeData';
-import { getDatabaseCart } from '../../utilities/databaseManager';
+import { getDatabaseCart, processOrder, removeFromDatabaseCart } from '../../utilities/databaseManager';
+import Cart from '../Cart/Cart';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import happyImage from '../../images/giphy.gif';
 
 const Review = () => {
 
     const [cart, setCart] = useState([]);
+    const [orderPlaced,setOrderPlaced]= useState(false);
+
+    const handlePlaceOrder = () =>{
+        setCart([]);
+        setOrderPlaced(true);
+        processOrder();
+    }
+
+    const removeProduct = (productKey) =>{
+        const newCart = cart.filter(pd=>pd.key !== productKey);
+        setCart(newCart);
+        removeFromDatabaseCart(productKey);
+    }
 
     useEffect(()=>{
         //cart
@@ -19,14 +34,29 @@ const Review = () => {
         setCart(cartProducts);
     },[])
 
+    let thankyou;
+    if(orderPlaced){
+        thankyou = <img src={happyImage} alt=""/>
+    }
+
     return (
-        <div>
-            <h1>Cart Items:{cart.length} </h1>
+        <div className="twin-container">
+           <div className="product-container">
             {
-                cart.map(pd => <ReviewItem 
-                    key = {pd.key}
-                    product={pd}></ReviewItem>)
+                    cart.map(pd => <ReviewItem 
+                        key = {pd.key}
+                        removeProduct={removeProduct}
+                        product={pd}></ReviewItem>)
             }
+            {
+                thankyou
+            }
+           </div>
+           <div className="cart-container">
+                <Cart cart={cart}>
+                    <button onClick={handlePlaceOrder} className="main-button">Place Order</button>
+                </Cart>
+           </div>
         </div>
     );
 };
