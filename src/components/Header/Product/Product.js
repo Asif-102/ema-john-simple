af08cb4b9bb1e5112322aa9ed5572b95872c2ffa
Asif2535 <aiqbal517@gmail.com>
2021-03-08 02:11:04 +0600

    know more about destructuring

--- src/components/Header/Product/Product.js
+++ src/components/Header/Product/Product.js
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 
 const Product = (props) => {
     // console.log(props);
-    const { img, name, seller, price, stock, key } = props.product;
+    const {product, handleAddProduct} = props;
+    const { img, name, seller, price, stock, key } = product;
     return (
         <div className="product">
             <div>
@@ -22,7 +23,7 @@ const Product = (props) => {
                 add to cart</button> */}
                 {props.showAddToCart && <button
                  className="main-button"
-                 onClick={()=>props.handleAddProduct(props.product)}
+                 onClick={()=>handleAddProduct(product)}
                  > 
                  <FontAwesomeIcon icon={faShoppingCart} />{' '}add to cart
                 </button>}
